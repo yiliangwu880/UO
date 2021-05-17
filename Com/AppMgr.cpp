e17bd76499814a3c2ba5face4a0b9f9c9c61d39c
yiliangwu880 <30390012@qq.com>
2021-05-17 21:03:35 +0800

    create

---
+++ Com/AppMgr.cpp
@@ -0,0 +1,51 @@
+#include "AppMgr.h"
+#include "su_mgr.h"
+#include "single_progress.h"
+#include "libevent_cpp/include/include_all.h"
+#include "libevent_cpp/include/http.h"
+#include "cfg.h"
+#include "svr.h"
+
+using namespace su;
+using namespace lc;
+
+class MyLcLog : public lc::ILogPrinter, public Singleton<MyLcLog>
+{
+public:
+	virtual void Printf(lc::LogLv lv, const char * file, int line, const char *fun, const char * pattern, va_list vp) override
+	{
+		su::LogMgr::Ins().Printf((su::LogLv)lv, file, line, fun, pattern, vp);
+	}
+};
+void MyPrintf(su::LogLv lv, const char * file, int line, const char *fun, const char * pattern)
+{
+	//su::LogMgr::Ins().Printf((su::LogLv)lv, file, line, fun, pattern);
+}
+
+void AppMgr::Start(int argc, char* argv[], const string &app_name)
+{
+	FireEvent<AE_CFG_INI>(); //配置初始化
+	SingleProgress::Ins().Check(argc, argv, app_name.c_str(), AppMgr::Ins().m_isDaemon); //启动关闭进程管理
+	lc::LogMgr::Ins().SetLogPrinter(MyLcLog::Ins());
+	//su::LogMgr::Ins().SetLogPrinter(MyPrintf);
+	SuMgr::Ins().Init();
+	lc::Timer timer; //一个进程只需要一个lc::Timer，其他定时器由  svr_util 驱动
+	auto f = std::bind(&AppMgr::OnTimer, this);
+	timer.StartTimer(30, f, true);
+
+	FireEvent<AE_AFTER_NET_INT>();
+	EventMgr::Ins().Dispatch();
+	L_INFO("main end");
+}
+
+void AppMgr::OnTimer()
+{
+	if (SingleProgress::Ins().IsExit())
+	{
+		L_INFO("OnExitProccess");
+		EventMgr::Ins().StopDispatch();
+		FireEvent<AE_ON_EXIT>();
+		return;
+	}
+	SuMgr::Ins().OnTimer();
+}
--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # UO
 uo server
 
+# 开发目的
+	演练一遍游戏实现，如何做得高效，代码质量高，可复用。
+	同时看有没有希望设计好玩的游戏。
+
 # tempCode
 临时伪代码，目的快速演练一遍整体代码设计模式。后期参考他构建代码
+
+# 目录介绍
+access_svr   --接入服
+Center		 --中心服
+Zone		 --场景服
+dbproxy_svr  --db代理服
+
+db_driver	 --dbproxy_svr的驱动库
+dbProto		 --db代理服和业务服协议
+CppProto	 --业务服之间协议
+Com			 --多个进程复用的源码
+bin			 --执行文件
