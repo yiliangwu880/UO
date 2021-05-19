af29475708a0f8e5217aa9f1e3b99db9f5860a47
yiliangwu880 <30390012@qq.com>
2021-05-19 14:00:09 +0800

    rename

---
+++ center/Event.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "game_util/publish_subscribe.h"
+//100开始
+enum Event
+{
+	EV_CFG_INI = 1,
+	EV_START = 2, 
+	EV_ON_EXIT = 3, //进程结束
+};
+
+namespace su
+{
+	//template<>
+	//struct EventTraits<Event::CFG_INI> {
+	//	using Fun = void(*)(); //默认事件函数类型，可以不用定义 EventTraits
+	//};
+
+}
---
+++ center/main.cpp
@@ -0,0 +1,34 @@
+#include "libevent_cpp/include/include_all.h"
+#include "svr_util/include/su_mgr.h"
+#include "svr_util/include/single_progress.h"
+#include "svr_util/include/log_file.h"
+#include "AppMgr.h"
+#include "Event.h"
+using namespace std;
+using namespace su;
+using namespace lc;
+
+class AppMgr : public BaseAppMgr
+{
+public:
+	virtual void OnBeforeStart() override
+	{
+		FireEvent<EV_CFG_INI>();
+	}
+	virtual void OnStart() override
+	{
+		FireEvent<EV_START>();
+	}
+	virtual void OnExit() override
+	{
+		FireEvent<EV_ON_EXIT>();
+	}
+};
+
+int main(int argc, char* argv[])
+{
+	AppMgr app;
+	app.Start(argc, argv, "center");
+	return 0;
+}
+
